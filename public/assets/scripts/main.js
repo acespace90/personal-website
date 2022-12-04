80f822b4e51ce6d6d56d9c2947bc3d26bce08bae
Antonio Matera <antoniomatera1990@gmail.com>
2022-12-04 14:41:37 +0100

    Saves dark mode state into localstorage

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -3,4 +3,11 @@ const htmlRootEl = document.querySelector('html');
 
 btnThemeSwitcher.addEventListener('click', () => {
   htmlRootEl.classList.toggle('dark');
+  if (htmlRootEl.classList.contains('dark')) {
+    localStorage.setItem('dark', "true");
+  } else {
+    localStorage.setItem('dark', "false");
+  }
+
 });
+
