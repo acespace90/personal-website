87973dc67970ba8d161c0dde6ac1c824116da27f
Antonio Matera <antoniomatera1990@gmail.com>
2022-12-04 18:34:11 +0100

    Adds date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
-module.exports = function(eleventyConfig) {
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
   // Output directory: _site
 
   eleventyConfig.addPassthroughCopy('./assets/img');
   eleventyConfig.addPassthroughCopy('./assets/css/main.css');
   eleventyConfig.addPassthroughCopy('./assets/scripts/main.js');
   eleventyConfig.addPassthroughCopy('./src/admin');
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
