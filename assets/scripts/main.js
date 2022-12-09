cfc6bd1ff09c0e388dd0d7f051a930d01d8a7e3c
Antonio Matera <antoniomatera1990@gmail.com>
2022-12-09 15:00:41 +0100

    Toggles chrome color via js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,15 @@
 const btnThemeSwitcher = document.querySelector('.c-nav__theme-btn');
 const htmlRootEl = document.querySelector('html');
+const metaThemeColorToggle = document.querySelector('meta[name="theme-color"]');
 
 btnThemeSwitcher.addEventListener('click', () => {
   htmlRootEl.classList.toggle('dark');
   if (htmlRootEl.classList.contains('dark')) {
     localStorage.setItem('dark', "true");
+    metaThemeColorToggle.setAttribute('content',  '#000000');
   } else {
     localStorage.setItem('dark', "false");
+    metaThemeColorToggle.setAttribute('content',  '#EEE2DA');
   }
 
 });
