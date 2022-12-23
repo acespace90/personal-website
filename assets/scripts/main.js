86d70409fef2d2acd12e364b8ceea2824fc1b16f
Antonio Matera <antoniomatera1990@gmail.com>
2022-12-23 12:24:02 +0100

    Removes js of sticky header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,35 +13,3 @@ btnThemeSwitcher.addEventListener('click', () => {
   }
 
 });
-
-window.addEventListener("load", function(){
-  const $body = document.querySelector("body")
-  let scrollDirectionDown = true
-
-  window.addEventListener('scroll', () => {
-    if (window.oldScroll > window.scrollY) {
-      scrollDirectionDown = false
-    } else {
-      scrollDirectionDown = true
-    }
-    window.oldScroll = window.scrollY
-
-    if (scrollDirectionDown) {
-      $body.classList.remove("js-scrolling-up")
-      $body.classList.add("js-scrolling-down")
-    } else {
-      $body.classList.remove("js-scrolling-down")
-      $body.classList.add("js-scrolling-up")
-    }
-    if (window.scrollY > 600) {
-      $body.classList.remove("js-sticky-pre-zone")
-      $body.classList.add("js-sticky-zone")
-    } else if (window.scrollY > 300) {
-      $body.classList.remove("js-sticky-zone")
-      $body.classList.add("js-sticky-pre-zone")
-    } else {
-      $body.classList.remove("js-sticky-pre-zone")
-      $body.classList.remove("js-sticky-zone")
-    }
-  })
-});
